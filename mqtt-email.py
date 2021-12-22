71fbf5d5c9bc646065f32fb01eb6748fcb7e5418
rapi3 <18531150+rapi3@users.noreply.github.com>
2021-12-22 19:08:07 +0100

    Create mqtt-email.py

---
+++ mqtt-email.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# add in /etc/aliases
+# usercctv: |/etc/postfix/script/mqtt-email.py
+# Don't forget to: postalias /etc/aliases and afterwards reload Postfix with postfix reload
+
+import sys
+import re
+import paho.mqtt.publish as publish
+
+matching_string = "QWxhcm0gRXZlbnQ6IE1vdGlvbiBEZXRlY3Rpb24gU3RhcnQ"
+
+sender = "name"
+
+def find_sender(message):
+    global sender
+    sender = re.search(r"(?<=Return-Path: <).+?(?=\@)", message)
+    sender = sender.group()
+
+def parse():
+    message = sys.stdin.read()
+
+    if matching_string in message:
+        find_sender(message)
+        publish.single('IOT/cctv/{}'.format(sender), "Alarm", hostname="192.168.1.133", client_id="cctv-email", auth = {'username':"mqtt-email", 'password':"your_pass"})
+
+if __name__ == '__main__':
+    parse()
